63d64283d62cf4fefb819b2cc3fe2c8959e819b7
Yashprit Singh <yashprits@gmail.com>
2015-03-12 22:14:57 +0530

    modified file strcuture

--- app/js/ui/event.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#saveConnection").on("click", function(){
-        var ip = $("#ip").val();
-        var port = $("#port").val();
-        require("./js/index")(ip, port);
-    });
-});
